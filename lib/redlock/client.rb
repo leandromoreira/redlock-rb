6f6fd896fd7f992ca3c65d18fdf8e120295d4de7
Joel Korpela <syright@gmail.com>
2021-07-20 09:52:17 -0700

    precalculate the SHA1 hashes when loading Redlock's module

--- lib/redlock/client.rb
+++ lib/redlock/client.rb
@@ -3,6 +3,8 @@ require 'securerandom'
 require 'digest'
 
 module Redlock
+  include Scripts
+
   class Client
     DEFAULT_REDIS_HOST    = ENV["DEFAULT_REDIS_HOST"] || "localhost"
     DEFAULT_REDIS_PORT    = ENV["DEFAULT_REDIS_PORT"] || "6379"
@@ -143,27 +145,6 @@ module Redlock
     private
 
     class RedisInstance
-      UNLOCK_SCRIPT = <<-eos
-        if redis.call("get",KEYS[1]) == ARGV[1] then
-          return redis.call("del",KEYS[1])
-        else
-          return 0
-        end
-      eos
-
-      # thanks to https://github.com/sbertrang/redis-distlock/blob/master/lib/Redis/DistLock.pm
-      # also https://github.com/sbertrang/redis-distlock/issues/2 which proposes the value-checking
-      # and @maltoe for https://github.com/leandromoreira/redlock-rb/pull/20#discussion_r38903633
-      LOCK_SCRIPT = <<-eos
-        if (redis.call("exists", KEYS[1]) == 0 and ARGV[3] == "yes") or redis.call("get", KEYS[1]) == ARGV[1] then
-          return redis.call("set", KEYS[1], ARGV[1], "PX", ARGV[2])
-        end
-      eos
-
-      PTTL_SCRIPT = <<-eos
-        return { redis.call("get", KEYS[1]), redis.call("pttl", KEYS[1]) }
-      eos
-
       module ConnectionPoolLike
         def with
           yield self
@@ -181,13 +162,11 @@ module Redlock
           end
           @redis.extend(ConnectionPoolLike)
         end
-
-        generate_script_shas
       end
 
       def lock(resource, val, ttl, allow_new_lock)
         recover_from_script_flush do
-          @redis.with { |conn| conn.evalsha @lock_script_sha, keys: [resource], argv: [val, ttl, allow_new_lock] }
+          @redis.with { |conn| conn.evalsha Scripts::LOCK_SCRIPT_SHA, keys: [resource], argv: [val, ttl, allow_new_lock] }
         end
       rescue Redis::BaseConnectionError
         false
@@ -195,7 +174,7 @@ module Redlock
 
       def unlock(resource, val)
         recover_from_script_flush do
-          @redis.with { |conn| conn.evalsha @unlock_script_sha, keys: [resource], argv: [val] }
+          @redis.with { |conn| conn.evalsha Scripts::UNLOCK_SCRIPT_SHA, keys: [resource], argv: [val] }
         end
       rescue
         # Nothing to do, unlocking is just a best-effort attempt.
@@ -203,7 +182,7 @@ module Redlock
 
       def get_remaining_ttl(resource)
         recover_from_script_flush do
-          @redis.with { |conn| conn.evalsha @pttl_script_sha, keys: [resource] }
+          @redis.with { |conn| conn.evalsha Scripts::PTTL_SCRIPT_SHA, keys: [resource] }
         end
       rescue Redis::BaseConnectionError
         nil
@@ -211,14 +190,14 @@ module Redlock
 
       private
 
-      def generate_script_shas
-        @unlock_script_sha = Digest::SHA1.hexdigest(UNLOCK_SCRIPT)
-        @lock_script_sha   = Digest::SHA1.hexdigest(LOCK_SCRIPT)
-        @pttl_script_sha   = Digest::SHA1.hexdigest(PTTL_SCRIPT)
-      end
-
       def load_scripts
-        [UNLOCK_SCRIPT, LOCK_SCRIPT, PTTL_SCRIPT].each do |script|
+        scripts = [
+          Scripts::UNLOCK_SCRIPT,
+          Scripts::LOCK_SCRIPT,
+          Scripts::PTTL_SCRIPT
+        ]
+
+        scripts.each do |script|
           @redis.with { |conn| conn.script(:load, script) }
         end
       end
